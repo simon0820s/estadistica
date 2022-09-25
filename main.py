29a3243d0a5187a3ab3ca2c6467832c238a2275c
simon0820s <simon0820s@gmail.com>
2022-09-24 22:28:09 -0500

    creacion de clase y main, adicion de gitignore

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hola")
+from tabClass import Tabla
+def main():
+    lista=[]
+    n=int(input("cuantos datos desea ingresar? "))
+
+    for i in range(0,n):
+        ent=input("ingrese el dato #"+str(i+1)+" ")
+        lista.append(ent)
+
+    tabla=Tabla(lista)
+    print(tabla.printTab())
+
+
+if __name__=='__main__':
+    main()
+
