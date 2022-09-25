490164964664d6a7c8709038337139fe65caafb6
simon0820s <simon0820s@gmail.com>
2022-09-25 09:13:25 -0500

    creacion de el atributo name y prueba del mismo

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from tabClass import Tabla
 def main():
     lista=[]
+    name=input("ingrese el nombre de su tabla: ")
     n=int(input("cuantos datos desea ingresar? "))
 
     for i in range(0,n):
         ent=input("ingrese el dato #"+str(i+1)+" ")
         lista.append(ent)
 
-    tabla=Tabla(lista)
+    tabla=Tabla(lista,name)
     print(tabla.printTab())
 
 
--- tabClass.py
+++ tabClass.py
@@ -1,9 +1,21 @@
 class Tabla:
     #Constructor
-    def __init__(self,tabla=[]):
+    def __init__(self,tabla=[],name=""):
+        self._name=name
         self._tabla=tabla
+    
+    #Metodos setter y getter Name
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self,value):
+        self._name=value
+    @name.deleter
+    def name(self):
+        del self._name
 
-    #Metodos setter y getter
+    #Metodos setter y getter Tabla
     @property
     def tabla(self):
         return self._tabla
@@ -18,4 +30,5 @@ class Tabla:
 
     #print data
     def printTab(self):
-        return(f'''Tabla: {self.tabla}''')
+        return(f'''Tabla: {self._name}:
+ {self.tabla}''')
